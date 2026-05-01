24faac4ae102d3c13830ad38c66749dd7ea46f91
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 19:20:15 +0000

    add Weather Station Footprint (customSmallerIsBetter) benchmark result for b70d341f4580329e69989818497d7c15fe26644b

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777663215448,
+  "repoUrl": "https://github.com/tobiwan88/weather-station",
+  "entries": {
+    "Weather Station Footprint": [
+      {
+        "commit": {
+          "author": {
+            "email": "tobiuhg@gmail.com",
+            "name": "Tobias Meyer",
+            "username": "tobiwan88"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b70d341f4580329e69989818497d7c15fe26644b",
+          "message": "Merge pull request #23 from tobiwan88/feat/sensor-node-pipe-transport\n\nfeat: add ROM/RAM footprint benchmarking",
+          "timestamp": "2026-05-01T21:15:37+02:00",
+          "tree_id": "ee465a129012cdf1e33d498e23527df77a7de30e",
+          "url": "https://github.com/tobiwan88/weather-station/commit/b70d341f4580329e69989818497d7c15fe26644b"
+        },
+        "date": 1777663215007,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "gateway-frdm-mcxn947 / ROM / Total",
+            "value": 0,
+            "unit": "bytes"
+          },
+          {
+            "name": "gateway-frdm-mcxn947 / RAM / Total",
+            "value": 0,
+            "unit": "bytes"
+          }
+        ]
+      }
+    ]
+  }
+}
